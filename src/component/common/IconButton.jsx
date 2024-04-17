8fa67d3451af688e5b4651a9333f6867938d5253
anujYadav <anujvns28@gmail.com>
2024-04-17 23:30:21 +0530

    lecture viewing feture done

--- src/component/common/IconButton.jsx
+++ src/component/common/IconButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const IconButton = ({text,active,handlear,children,type}) => {
+const IconButton = ({text,active,handlear,children,type,customClasses}) => {
   return (
     <button onClick={handlear} type={type}
       className={`flex items-center cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 
+      ${customClasses}
       ${
         !active ? "bg-richblack-200" : "bg-yellow-50"
       }`}
--- src/component/core/viewCourse/VideoDetail.jsx
+++ src/component/core/viewCourse/VideoDetail.jsx
@@ -1,12 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { fetchSubSection, getFullDetailsOfCourse } from '../../../service/operation/Course';
+import IconButton from '../../common/IconButton';
 
 const VideoDetail = () => {
   const { courseId ,lectureId} = useParams();
   const { token } = useSelector((state) => state.auth);
   const [lectureData,setLectureData] = useState();
+  const [courseData,setCourseData] = useState();
+  const [allLecture,setAllLecture] = useState([]);
+  const [isRunningLecture,setIsRunningLecture] = useState();
+
+  const navigate = useNavigate()
+  const videoRef = useRef();
   
   const fetchLectureData = async () => {
     const result = await fetchSubSection({subSectionId:lectureId}, token);
@@ -15,24 +22,97 @@ const VideoDetail = () => {
     }
   }
 
+  const fetchCourseData = async() => {
+    const result = await getFullDetailsOfCourse(courseId, token);
+    let lectureArr = []
+    if (result) {
+       setCourseData(result.courseDetails);
+       result.courseDetails.courseContent.map((section) => {
+        if(section.subSection.length > 0){
+          section.subSection.map((subSection) => {
+            lectureArr.push(subSection._id);
+           })
+        }
+       })
+       setAllLecture(lectureArr)
+    }
+  }
+
+  const lectureIndex = () => {
+    let arr = [...allLecture] 
+    return arr.findIndex((item) => item== lectureId)
+  }
+
+  const rewatch = () => {
+    videoRef.current.play();
+    setIsRunningLecture(false)
+  }
+
+  const nextLecture = (i) => {
+    navigate(`/view-course/${courseId}/${allLecture[i+1]}`)
+    setIsRunningLecture(false)
+  }
+
+  const prevLecture = (i) => {
+    navigate(`/view-course/${courseId}/${allLecture[i-1]}`)
+    setIsRunningLecture(false)
+  }
+
   useEffect(() => {
     fetchLectureData();
   }, [lectureId])
 
-console.log(lectureData,"this is lecture data")
+  useEffect(() => {
+  fetchCourseData();
+  },[])
+
+ console.log(lectureIndex(),"lecture index")
+
   return (
-    <div className='w-full h-full flex items-center justify-center'>
+    <div className='w-full h-full flex items-center justify-center  flex-col gap-5 text-white'>
       {
         !lectureData ? <div className='text-white'>Loading...</div>
         : <div className='w-full h-full overflow-y-hidden'>
           <video
+           ref={videoRef}
            className='w-full h-full'
            src={lectureData.videoUrl}
            autoPlay
            controls
-           onEnded={() => console.log("ended ji")}
+           onEnded={() => setIsRunningLecture(true)}
           />
         </div>
+      }
+      {
+        isRunningLecture && <div
+        
+        className="w-full  border-solid border-black absolute "
+      >     
+        <IconButton
+          text="Rewatch"
+          active={true}
+          handlear={() => rewatch()}
+          customClasses="text-xl max-w-max px-4 mx-auto mt-2"
+        />
+        <div className="mt-10 flex min-w-[250px] justify-center gap-x-4 text-xl">
+          {  lectureIndex() > 0 && (
+            <button
+              onClick={() =>prevLecture(lectureIndex())}
+              className="cursor-pointer rounded-md bg-richblack-800 px-[20px] py-[8px] font-semibold text-richblack-5"
+            >
+              Prev
+            </button>
+          )}
+          {lectureIndex() < allLecture.length-1 && (
+            <button
+            onClick={() => nextLecture(lectureIndex())}
+              className="cursor-pointer rounded-md bg-richblack-800 px-[20px] py-[8px] font-semibold text-richblack-5"
+            >
+              Next
+            </button>
+          )}
+        </div>
+      </div>
       }
     </div>
   )
